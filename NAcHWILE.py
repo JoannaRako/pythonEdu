56d3421ad20939d2ef12bb72f6e24aa22e8f6292
JoannaRako <joannarakoczyy@gmail.com>
2022-11-12 20:57:11 +0100

    next

--- NAcHWILE.py
+++ NAcHWILE.py
@@ -1,18 +1,3 @@
-# dodawanie wartosci jest szybsze w slowniku, nie liczy sie kolejnosc
-# dictionaries in dictionary
-
-people = {
-          "jdhIhjojJIojiOJIJuh1":{ 'name': 'John', 'age': 27, 'sex': 'Male' },
-          "jdhIhjojJIojiOJIJuh2":{ 'name': 'Ben', 'age': 24, 'sex': 'Male' },
-          "jdhIhjojJIojiOJIJuh3":{ 'name': 'Ren', 'age': 76, 'sex': 'Female' },
-          "jdhIhjojJIojiOJIJuh4":{ 'name': 'Ted', 'age': 85, 'sex': 'Male' },
-          "jdhIhjojJIojiOJIJuh5":{ 'name': 'Ross', 'age': 34, 'sex': 'Male' },
-          "jdhIhjojJIojiOJIJuh6":{ 'name': 'Monica', 'age': 56, 'sex': 'Female' },
-          "jdhIhjojJIojiOJIJuh7":{ 'name': 'Rachel', 'age': 54, 'sex': 'Female' }
-         }
-'''
-# list with dictionaries
-
 peopleList = [
         {'id': 'gdfNJKHuUUUh', 'name': 'John', 'age': 27, 'sex': 'Male'},
         {'id': 'HUiuUIuUIUhg', 'name': 'Ben', 'age': 24, 'sex': 'Male'},
@@ -21,20 +6,9 @@ peopleList = [
         {'id': 'jiojijiojojU', 'name': 'Ross', 'age': 34, 'sex': 'Male'}
         ]
 
-ratings = {
-        "Arkadiusz": (3, 3, 3, 4, 4, 5),
-        "Beata": (3, 3, 3, 4, 4, 5),
-        }
-
-for key in ratings:           
-    print (key, "oceny", ratings[key]) 
-
 for value in peopleList:
   for key in value:
     print(key, value[key])
   print()
 
-'''
 
-for key in people:
-    print(key)
