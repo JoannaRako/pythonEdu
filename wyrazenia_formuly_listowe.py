2fe8d63cc5943cf11977f7c93d8fe7770fcbfdcc
JoannaRako <joannarakoczyy@gmail.com>
2022-11-13 19:59:28 +0100

    new file

--- wyrazenia_formuly_listowe.py
+++ wyrazenia_formuly_listowe.py
@@ -11,18 +11,25 @@ for element in liczby:
 
 # szybsza wersja
 
-potegiDwojki2 = [element**2 for element in liczby]
+potegi1 = [element**2 for element in liczby]
 
 # można też napisać
 
-potegiDwojki2 = [element**2                 # CO WYKONUJEMY
-                 for elemenet in liczby     # SKĄD POBIERAMY
-                ]
+potegi2 = [element ** 2                 # CO WYKONUJEMY
+        for elemenet in liczby     # SKĄD POBIERAMY
+        ]
 
-liczParzyste2 = [element                   # CO WYKONUJEMY - nic
-                for element in liczby      # SKĄD POBIERAMY
-                if (element % 2 == 0)      # WARUNEK
-                ]
+parzyste2 = [element                   # CO WYKONUJEMY - nic
+            for element in liczby      # SKĄD POBIERAMY
+            if (element % 2 == 0)      # WARUNEK
+            ]
+
 '''
 co_zrobic_na_elemencie  for element in staraLista   warunek oparty na elemencie
 '''
+
+liczby = [1, 2, 3, 4, 5, 6]
+
+potegiRange = [element**2                   # CO WYKONUJEMY
+              for elemenet in range(20)     # SKĄD POBIERAMY
+              ]
