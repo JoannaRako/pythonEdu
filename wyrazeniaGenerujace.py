654f3638fad7f86b90d42db8b2ae7f482a2de1a3
JoannaRako <joannarakoczyy@gmail.com>
2022-11-14 17:13:00 +0100

    next

---
+++ wyrazeniaGenerujace.py
@@ -0,0 +1,11 @@
+evenNumbers =   [element
+                for element in range(400)
+                if (element % 2 == 0)
+                ]
+
+evenNumbersGenerator = (element
+                        for element in range (400)
+                        if (element % 2 == 0)
+                        )
+
+)
