de80fbe61980e25261b82a17773b4a4164d508b9
JoannaRako <joannarakoczyy@gmail.com>
2022-11-14 17:45:15 +0100

    next

--- dictionaryForm.py
+++ dictionaryForm.py
@@ -2,7 +2,7 @@
 
 names = {"Arkadiusz", "Beata", "Jarek", "Kacper"}
 numbers = [1, 2, 3, 4, 5, 6]
-celciua = {'t1': -20, 't2': -15, 't3': 12, 't4': f4, 't5': 0}
+celcius = {'t1': -20, 't2': -15, 't3': 12, 't4': f4, 't5': 0}
 
 namesLength = {
     1 : 2
