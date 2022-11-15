826a3b7246c43fce42b4de57798d829a3a79161f
JoannaRako <joannarakoczyy@gmail.com>
2022-11-15 15:12:14 +0100

    fixes

--- dictionaryExpressions.py
+++ dictionaryExpressions.py
@@ -6,14 +6,38 @@ celsius = {'t1': -20, 't2': -15, 't3': 0, 't4': 12, 't5': 24}
 
 # różni się od wyrażenia listownego tym, że podajemy co ma się stać dla klucz i wartość
 
+fahrenheit = {
+    temperature: ((degrees * 9/5) + 32)
+    for temperature, degrees in celsius.items()          # .items() - zmienia klucz/wartość w tuples
+}
+
+fahrenheit5 = {
+    temperature: ((celsius * 9/5) + 32)
+    for temperature, celsius in celsius.items()
+    if celsius > -5
+    if celsius < 20
+}
+
+print(fahrenheit)
+print(fahrenheit5)
+
+# -------------------------
+
 multipliedNumbers = {
     number: number*number
     for number in numbers
 }
+
+powerOFNumbers = {
+    number: number**number
+    for number in numbers
+}
+
 print(multipliedNumbers)
+print(powerOFNumbers)
 
+# -------------------------
 
-'''
 namesLength = {
     name: len(name)       # 1 - klucz; 2 - wart
     for name in names
@@ -22,6 +46,7 @@ namesLength = {
 >>> namesLength
 {'Wioletta': 8, 'Jakub': 5, 'Karol': 5, 'Bartłomiej': 10, 'Ardkadiusz': 10}
 '''
+
 
 namesLength2 = {
     name: len(name)       # 1 - klucz; 2 - wart
@@ -35,4 +60,3 @@ namesLength2 = {
 
 # można też:
 namesLength2 = {name: len(name) for name in names   if name.startswith("A")}
-'''
