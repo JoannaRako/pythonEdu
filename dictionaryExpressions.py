bd84c79a9bf78772c9b9c193ab57e249263aebc9
JoannaRako <joannarakoczyy@gmail.com>
2022-11-15 14:28:41 +0100

    fixes

---
+++ dictionaryExpressions.py
@@ -0,0 +1,9 @@
+# wyrażenia słownikowe
+
+names = {"Ardkadiusz", "Wioletta", "Karol", "Bartłomiej", "Jakub"}
+numbers = [1, 2, 3, 4, 5, 6]
+celsius = {'t1': -20, 't2': -15, 't3': 0, 't4': 12, 't5': 24}
+
+namesLenght = {
+
+}
