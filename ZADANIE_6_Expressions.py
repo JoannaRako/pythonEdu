1a013a3fc3173771b15cc4ac4dd578d2e109871d
JoannaRako <joannarakoczyy@gmail.com>
2022-11-16 13:13:33 +0100

    homework

---
+++ ZADANIE_6_Expressions.py
@@ -0,0 +1,36 @@
+'''
+Znajdz liczby od 100 do 470 włącznie, które są:
+- podzielne prze 7, ale nie są podzielne przez 5
+
+1) generator
+2) wyrazenie listowne
+3) wyrazenie zbioru
+4) wyrazenie słownikowe
+
+'''
+
+# 1) generator
+
+evenNumbersGenerator = (element
+                        for element in range(100, 470)
+                        if (element % 7 == 0)
+                        if not (element % 5 !== 0)
+                        )
+
+print("Generator:\n")
+for item in evenNumbersGenerator:
+    print(item)
+
+# 2) wyrazenie listowne
+
+listGenerator = [element                            # CO WYKONUJEMY - nic
+                for element in range(100, 470)      # SKĄD POBIERAMY
+                if (element % 7 == 0)
+                if not (element % 5 != 0)
+                ]
+
+print("Listowne:\n")
+for item in listGenerator:
+    print(item)
+
+# 3) wyrazenie listowne
