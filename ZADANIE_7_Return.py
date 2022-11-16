341d96c3daaf367f0ab910558b302d1d2663fb2b
JoannaRako <joannarakoczyy@gmail.com>
2022-11-16 18:14:41 +0100

    homework

--- ZADANIE_7_Return.py
+++ ZADANIE_7_Return.py
@@ -1,56 +1,13 @@
 import math
 
-# WYBÓR
+# WYBÓR:
 
-choose = (input("""
-        Powierzchnię jakiej figury chcesz policzyć?
-        1) prostokąta
-        2) kwadratu
-        3) trójkąta
-        4) trapezu
-        5) koła
-"""))
-
-# OPCJA
-
-if choose == 1:
-
-    print("Wprowadź długość i szerokość:\n")
-    a = int(input("Długość: "))
-    b = int(input("Szerokość: "))
-
-    pole_prostokata(a, b)
-
-elif choose == 2:
-
-    print("Wprowadź długość boku:\n")
-    a = int(input("Długość boku: "))
-
-    pole_kwadratu(a)
-
-elif choose == 3:
-
-    print("Wprowadź długość podstawy i wysokość\n")
-    a = int(input("Długość podstawy: "))
-    h = int(input("Wysokość: "))
-
-    pole_trojkata(a, h)
-
-elif choose == 4:
-
-    print("Wprowadź długość podstawy i wysokość lub długość podstawy \n")
-    a = int(input())
-    b = int(input())
-    h = int(input())
-
-    pole_trapezu(a, b, h)
-
-elif choose == 5:
-
-    r = int(input("Wprowadź promień: "))
-    pole_kola(r)
-else:
-    print("Nie rozumiem")
+choose = (input("Powierzchnię jakiej figury chcesz policzyć?\n"
+                "1) prostokąta\n"
+                "2) kwadratu\n"
+                "3) trójkąta\n"
+                "4) trapezu\n"
+                "5) koła\n"))
 
 # FUNKCJE:
 
@@ -71,6 +28,46 @@ def pole_trapezu(a, b, h):
 def pole_kola(r):
 
     return math.pi * r ** 2
+
+
+# OPCJA DO WYBORU:
+
+if (choose == '1'):
+
+    a = int(input("Długość:\n"))
+    b = int(input("Szerokość:\n"))
+
+    print(pole_prostokata(a, b))
+
+elif choose == '2':
+
+    a = int(input("Długość boku:\n"))
+
+    print(pole_kwadratu(a))
+
+elif choose == '3':
+
+    a = int(input("Długość podstawy:\n"))
+    h = int(input("Wysokość:\n"))
+
+    print(pole_trojkata(a, h))
+
+elif choose == '4':
+
+    a = int(input("Długość podstawy\n"))
+    h = int(input("Wysokość\n"))
+    b = int(input("Długość podstawy\n"))
+
+    print(pole_trapezu(a, b, h))
+
+elif choose == '5':
+
+    r = int(input("Wprowadź promień:\n"))
+    print(pole_kola(r))
+
+else:
+    print("Nie rozumiem")
+
 
 
 
