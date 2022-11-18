d4fba61f862e0d7403bf2c596a228c4565c922fd
Adrian <adilos1234@o2.pl>
2022-11-18 12:08:10 +0100

    49. Lesson

---
+++ multiModelApp.py
@@ -0,0 +1,10 @@
+'''
+Moduł - plik z kodem;
+# część do wykorzystania czegoś większego
+np. biblioteka (złożenie kilku modułów; zestaw narzędzi)
+
+3 typy modułów:
+    standartowa biblioteka (do ściągnięcia w każdej chwili)
+    zewnętrzne źródła
+    napisane przez nas
+'''
