ec9005d16c6be849e9ce2bcdee037b1b6a9b057f
Adrian <adilos1234@o2.pl>
2022-11-18 13:09:35 +0100

    50. Lesson

--- clculationsAndEnum.py
+++ clculationsAndEnum.py
@@ -1,41 +1,57 @@
 import calculationsModule
 
 # enumeration - spis - wyliczenie
-from enum import Enum
 
-Menu = Enum('Menu', 'Prostokąt Kwadrat Trójkąt Trapez Koło')
+from enum import IntEnum
+
+Menu = IntEnum('Menu', 'Prostokąt Kwadrat Trójkąt Trapez Koło')
+
+# Menu = IntEnum('Menu', 'Prostokąt, Kwadrat, Trójkąt, Trapez, Koło')
+# Menu = IntEnum('Menu', ['Prostokąt', 'Kwadrat', 'Trójkąt', 'Trapez', 'Koło'])
+# Menu = IntEnum('Menu', {'Prostokąt':11, 'Kwadrat':12, 'Trójkąt':13, 'Trapez':14, 'Koło':15]}
+
 # jak korzystasz z enumeracji zmienne z duzych liter
 
 
-choose = input("""Wybierz pole której figury chcesz obliczyć?
+choose = int(input("""Wybierz pole której figury chcesz obliczyć?
                 1. Prostokąt
                 2. Kwadrat
                 3. Trójkąt
                 4. Trapez
                 5. Koło
-                """)
+                """))
 
-if (choose == '1'):
+'''
+ALBO 
+from enum import Enum
+Enum + if (choose == Menu.Prostokąt.value):
+
+ALBO
+from enum import IntEnum
+IntEnum + if (choose == Menu.Prostokąt
+'''
+
+if (choose == Menu.Prostokąt.value):
 
     a = float(input("Długość:\n"))
     b = float(input("Szerokość:\n"))
 
     print("Pole wynosi:", calculationsModule.pole_prostokata(a, b))
 
-elif choose == '2':
+elif choose == Menu.Kwadrat:
 
     a = float(input("Długość boku:\n"))
 
     print("Pole wynosi:", calculationsModule.pole_kwadratu(a))
 
-elif choose == '3':
+elif choose == Menu.Trójkąt:
 
     a = float(input("Długość podstawy:\n"))
     h = float(input("Wysokość:\n"))
 
     print("Pole wynosi:", calculationsModule.pole_trojkata(a, h))
 
-elif choose == '4':
+elif choose == Menu.Trapez:
 
     a = float(input("Długość podstawy\n"))
     h = float(input("Wysokość\n"))
@@ -43,7 +59,7 @@ elif choose == '4':
 
     print("Pole wynosi:", calculationsModule.pole_trapezu(a, b, h))
 
-elif choose == '5':
+elif choose == Menu.Koło:
 
     r = float(input("Wprowadź promień:\n"))
     print("Pole wynosi:", calculationsModule.pole_kola(r))
