12bcf197eb3e72b7c5147eaf205436cd9e952dca
JoannaRako <joannarakoczyy@gmail.com>
2022-11-20 22:04:35 +0100

    Fixses

---
+++ ZADANIE_2_AbsoluteValue.py
@@ -0,0 +1,8 @@
+wartBezwgledna = int(input("""
+Podaj jakas liczbe co chcesz zamienic na bezwgledna wartosc: 
+"""))
+
+wartBezwgledna = abs(wartBezwgledna)
+
+print(wartBezwgledna)
+
--- ZADANIE_2_wartoscBezwzgledna.py
+++
@@ -1,8 +0,0 @@
-wartBezwgledna = int(input("""
-Podaj jakas liczbe co chcesz zamienic na bezwgledna wartosc: 
-"""))
-
-wartBezwgledna = abs(wartBezwgledna)
-
-print(wartBezwgledna)
-
