6a4d298d341896ff7ac5c59cf7e2811dae9d38e0
JoannaRako <joannarakoczyy@gmail.com>
2022-11-21 16:54:51 +0100

    52.

--- timeModule.py
+++ timeModule.py
@@ -1,3 +1,6 @@
+import time
+
+# Zmienna do wszystkiego
 number = int(input("Wpisz liczbę do której mam sumować: \n"))
 
 '''
@@ -8,40 +11,67 @@ def sumUp(number):
         sum = sum + number
         print("Dla liczby", number, "Wartość sumowania to:", sum)
     print(sum)
-sumUp(number)
 '''
 
 # 2. Pętlą
 def sumUp_for(number):
     sum = 0
-
     for number in range(1, number+1):
         sum = sum + number
     return sum
-print("2:", sumUp_for(number))
 
 
 # 3. Wyrażeniem listy
 def sumUp_generator(number):
     return sum([number for number in range(1, number+1)])
-print("3:", sumUp_generator(number))
 
 
 # 4. Wyrażeniem zbioru
 def sumUp_generator(number):
     return sum((number for number in range(1, number+1)))
-print("4:", sumUp_generator(number))
 
 
 # 5. Wyrażeniem słownikowym
 def sumUp_generator(number):
     return sum({number for number in range(1, number+1)})
-print("5:", sumUp_generator(number))
 
 
 # 6. Krótka wersja
-
 def sumUp_short(number):
     return (1 + number) / 2 * number
-print("6:", sumUp_short(number))
 
+'''
+start = time.perf_counter()
+sumUp(number)
+end = time.perf_counter()
+print("Czas wykonania tej funkcji to: ", end-start)            # Odjęcie czasu początkowego od końcowego
+'''
+
+# Funkcja mierzenia czasu
+
+def timeCount(start):
+    end = time.perf_counter()
+    return end-start
+
+start = time.perf_counter()
+print("2:", sumUp_for(number))
+print(timeCount(start))
+
+
+start = time.perf_counter()
+print("3:", sumUp_generator(number))
+print(timeCount(start))
+
+start = time.perf_counter()
+print("4:", sumUp_generator(number))
+print(timeCount(start))
+
+
+start = time.perf_counter()
+print("5:", sumUp_generator(number))
+print(timeCount(start))
+
+
+start = time.perf_counter()
+print("6:", sumUp_short(number))
+print(timeCount(start))
