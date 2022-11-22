fec12cf26fde0d53b3baef8717cf593028ad4c89
JoannaRako <joannarakoczyy@gmail.com>
2022-11-22 14:25:13 +0100

    56. Ćwiczenie

--- ZADANIE_9_ isElement.py
+++ ZADANIE_9_ isElement.py
@@ -1,18 +1,33 @@
 import time
 
-def function_performance(func, arg, how_many_times = 1):
+
+def function_performance(func, arg, how_many_times=1):
     sum = 0
 
     for i in range(0, how_many_times):
         start = time.perf_counter()
         func(arg)
         end = time.perf_counter()
-        sum = sum + (end-start)
+        sum = sum + (end - start)
 
     return sum
+
 
 setContainer = {i for i in range(1000)}
 listContainer = [i for i in range(1000)]
 
-def is_element(number):
-    if (number == True)
+element = int(input("Wprowadź element który chcesz sprawdzić\n"))
+
+
+def is_element_set(element):
+    if element in setContainer:
+        return True
+
+def is_element_list(element):
+    if element in listContainer:
+        return True
+
+
+print(is_element_list(element))
+
+print(is_element_set(element))
