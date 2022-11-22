e11154082c916dc3de5601fe05ace82c5e24ed8b
JoannaRako <joannarakoczyy@gmail.com>
2022-11-22 15:32:59 +0100

    57.

--- manyArguments.py
+++ manyArguments.py
@@ -3,11 +3,12 @@ import time
 # funkcja która sprawdzi jak szybko działa inna funkcja podaną lub domyślną ilość razy
 
 
-def function_performance (func, *arg, how_many_times = 1) :
+def function_performance(func, *arg, how_many_times = 1):
 
     sum = 0
     print(arg)
-    for i in range(0, how_many_times) :
+
+    for i in range(0, how_many_times):
         start = time.perf_counter()
         func(*arg)
         end = time.perf_counter()
@@ -22,11 +23,9 @@ listContainer = [i for i in range(10000)]
 element = int(input("Wprowadź element który chcesz sprawdzić\n"))
 
 
-def is_element_in(element) :
-    if element in container :
+def is_element_in(element, container):
+    if element in container:
         return True
 
-
-print(is_element_in(element, container))
-print(function_performance(is_element_in, element, setContainer , how_many_times))         # szybszy niż lista
-
+print(function_performance(is_element_in, element, setContainer, how_many_times = 5))
+print(function_performance(is_element_in, element, listContainer, how_many_times = 5))
