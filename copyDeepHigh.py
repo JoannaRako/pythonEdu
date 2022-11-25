b9e3bd197e43e6ad0e9dd88a44ac2d6121f90a3b
Adrian <adilos1234@o2.pl>
2022-11-25 16:56:14 +0100

    61. Lesson

--- copyDeepHigh.py
+++ copyDeepHigh.py
@@ -1,9 +1,12 @@
+import copy
+
 def evil_function(toBeDestroyedList):
 #   toBeDestroyedList.clear()
     toBeDestroyedList[0][0] = 20       # zmiana ID myList
 
 myList = [[1, 4], [2, 1], [3, 6], [4, 2]]
 
-# copy, modyfikacja typu bezpieczny oryginał - PŁYTKA KOPIA
+# copy, modyfikacja typu bezpieczny oryginał - COPY
 
 evil_function(myList.copy())
+evil_function(myList.deepcopy(myList))
